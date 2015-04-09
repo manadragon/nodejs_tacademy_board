4c1075b1761846c11989527e065829078a497c80
SungYeolWoo <altari1415@gmail.com>
2015-04-09 11:30:30 +0900

    db_board.js : connection pool setting

--- routes/board.js
+++ routes/board.js
@@ -4,6 +4,7 @@
 
 var express = require('express');
 var router = express.Router();
+var db_board = require('../models/db_board');
 
 /* GET board listing. */
 router.get('/', function(req, res, next) {
@@ -17,7 +18,25 @@ router.get('/write', function (req, res, next) {
 router.post('/write', function (req, res, next) {
   console.log('req.body', req.body);
 
+  var title = req.body.title;
+  var content = req.body.content;
+  var passwd = req.body.passwd;
+
+  var datas = [title, content, passwd];
+
   res.json({"result" : "success"});
+
+  db_board.write(datas, function (success) {
+
+    if (!success)
+      res.json({"result" : "fail"});  // 모바일 서버일 경우
+    else {
+      res.json({"result" : "success"});  // 모바일 서버일 경우
+      //res.redirect('/board/list/1');
+    }
+
+
+  });
 });
 
 module.exports = router;
